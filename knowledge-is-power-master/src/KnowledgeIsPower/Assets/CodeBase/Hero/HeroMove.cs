2e658462b144f460a957176fb388fcbb631f528a
IvanZhukouski <ivan.zhukouski.by@gmail.com>
2021-02-04 09:39:29 +0300

    added repos

---
+++ knowledge-is-power-master/src/KnowledgeIsPower/Assets/CodeBase/Hero/HeroMove.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using CodeBase.Infrastructure;
+using CodeBase.Services.Inputs;
+using UnityEngine;
+
+namespace CodeBase.Hero
+{
+    public class HeroMove : MonoBehaviour
+    {
+        private IInputService _inputService;
+        public CharacterController CharacterController;
+        public float MovementSpeed;
+        private Camera _camera;
+       
+        private void Awake()
+        {
+            _inputService = Game.InputService;
+            Debug.Log(_inputService.Axis);
+        }
+
+        public void Start()
+        {
+           // _inputService = Game.InputService;
+          //  Debug.Log(_inputService.Axis);
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            
+            Vector3 movementVector = Vector3.zero;
+           
+            if (_inputService.Axis.sqrMagnitude > Constants.Epsilon)
+            {
+                movementVector = _camera.transform.TransformDirection(_inputService.Axis);
+                movementVector.y = 0;
+                movementVector.Normalize();
+                transform.forward = movementVector;
+            }
+
+            movementVector += Physics.gravity;
+               
+            CharacterController.Move(movementVector * (MovementSpeed * Time.deltaTime));
+        }
+    }
+}
---
+++ knowledge-is-power-master/src/KnowledgeIsPower/Assets/CodeBase/Infrastructure/Game.cs
@@ -0,0 +1,26 @@
+﻿using CodeBase.Services.Inputs;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure
+{
+    public class Game
+    {
+        public static IInputService InputService;
+        public Game()
+        {
+            RegisterInputService();
+            
+        }
+
+        private static void RegisterInputService()
+        {
+            if (Application.isEditor)
+                InputService = new StandAloneInputService();
+            else
+            {
+                InputService = new MobileInputService();
+            }
+               
+        }
+    }
+}
